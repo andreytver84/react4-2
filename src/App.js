f60aab48dbd4af547f5f517e5a1c87af44ce2627
andreytver84 <uvikmedium@mail.ru>
2023-05-28 17:29:52 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -7,19 +7,17 @@ const INITIAL_LIST_DATA = [];
 
 function App() {
   const [listData, setListData] = useState(INITIAL_LIST_DATA);
-  const [sortListData, setSortListData] = useState(INITIAL_LIST_DATA);   
   const saveListDatHandler = (inputListData) => {
-    setListData(prevListData => [inputListData, ...prevListData]);
-    setSortListData(listData.sort((a,b) => a.date > b.date ? 1 : -1)); // сортировка данных по дате
+    setListData(prevListData => [inputListData, ...prevListData].sort((a,b) => a.date > b.date ? 1 : -1));
   }
   const removeItem = (item) => {
-    setSortListData(sortListData.filter(a => a !== item)); // удаление записи
+    setListData(listData.filter(a => a !== item)); // удаление записи
   }
   return (
     <div className="App">
       <AddToList onSaveListData={saveListDatHandler} />
       <div className='List' >
-        {sortListData.map((item, i) => <List onRemoveItem={removeItem} key={i} list={item} />)}
+        {listData.map((item, i) => <List onRemoveItem={removeItem} key={i} list={item} />)}
       </div>
     </div>
   );
